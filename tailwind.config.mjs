d15a89e528405dd406b3001416b4e5ecefef50d4
Joseph Watzson <49710538+joe-watson-sbf@users.noreply.github.com>
2023-10-27 17:10:46 -0500

    first commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'primary-light': '#ece7ea',
+				'primary-dark': '#241f1f',
+				'primary-blue': '#3949e3',
+				'primary-yellow': '#ffcb25',
+				'primary-slate': '#1e293b',
+			},
+		},
+	},
+	plugins: [],
+}
